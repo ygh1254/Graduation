2a5501bc0711111ed0065130d6c6c074e9e7d5fd
Harvey <harvey@infograb.net>
2025-11-23 21:24:03 +0900

    Allow Discord Externel Access

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
